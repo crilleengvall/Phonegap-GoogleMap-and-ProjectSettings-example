7c28fc1ba05f6c5e852d36bb40d188de421540d3
Christian Engvall <crilleengvall@gmail.com>
2012-04-08 17:36:16 +0200

    Added markers and zooming out map

--- www/googlemap.js
+++ www/googlemap.js
@@ -1,8 +1,36 @@
 function GoogleMap(){
     
     this.initialize = function(){
+        var map = showMap();
+        addMarkersToMap(map);
+    }    
+    
+    var addMarkersToMap = function(map){
+        var mapBounds = new google.maps.LatLngBounds();
+    
+        var latitudeAndLongitudeOne = new google.maps.LatLng('-33.890542','151.274856');
 
-        try{
+        var markerOne = new google.maps.Marker({
+					position: latitudeAndLongitudeOne,
+					map: map
+				});
+				
+        var latitudeAndLongitudeTwo = new google.maps.LatLng('57.77828', '14.17200');
+
+        var markerOne = new google.maps.Marker({
+					position: latitudeAndLongitudeTwo,
+					map: map
+				});
+				
+        mapBounds.extend(latitudeAndLongitudeOne);
+        mapBounds.extend(latitudeAndLongitudeTwo);
+        
+        map.fitBounds(mapBounds);
+    }
+    
+    
+    
+    var showMap = function(){
         var mapOptions = {
 			     zoom: 4,
 			     center: new google.maps.LatLng(-33, 151),
@@ -10,10 +38,7 @@ function GoogleMap(){
 			 }
 			 
         var map = new google.maps.Map(document.getElementById("mapArea"), mapOptions);
-        }
-        catch(error){
-            alert(error.message);
-        }
-    
+        
+        return map;
     }
 }
